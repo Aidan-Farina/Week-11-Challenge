58d78ecc77e2ca9ef153955b0ada99323b356979
Aidan <farinaaidan2@gmail.com>
2023-07-09 19:02:30 -0400

    added note route and inported helper from prior classwork

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const PORT = process.env.port || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
