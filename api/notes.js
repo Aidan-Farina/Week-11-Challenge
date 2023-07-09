0321e90b677f3c3ee3934d2e633aa5b734b1d7dd
Aidan <farinaaidan2@gmail.com>
2023-07-09 19:24:21 -0400

    got local server working

--- api/notes.js
+++ api/notes.js
@@ -1,2 +1,12 @@
 const express = require('express');
-const { readFromFile, writeToFile, readAndAppend } = require('./helper');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+
+const router = express.Router();
+const db = './db/db.json';
+
+router.get('/', (req, res) => {
+    const notes = readFromFile(db);
+    res.json(notes);
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const app = express();
+const notesRouter = require('./api/notes');
 
+const app = express();
 const PORT = process.env.port || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.use('/api/notes', notesRouter);
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
